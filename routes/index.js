1ab5624166514e111054c421fa1b5155a3dd79ec
bomsbro <transcend716@naver.com>
2020-11-09 16:19:13 +0900

    [init] 저장소 초기화

---
+++ routes/index.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+const mysql = require("mysql");
+var url = require("url");
+var urlJson;
+
+/*connect to mysql*/
+let client = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'votemanager',
+  password: '1234',
+  database: 'vote_db',
+  multipleStatements: true,
+});
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  urlJson=url.parse(req.url, true);
+  var chosen = urlJson.query.chosen;
+
+  var sql1="insert into vote_tb(choose) values("+chosen+");";
+  var sql2="select choose, count(*) as cnt from vote_tb group by choose;";
+  var sql;
+
+  if (typeof chosen == "undefined"){
+    sql=sql2;
+
+    client.query(sql, function(err, result, fields){
+      if(err){
+        console.log(err);
+        console.log("쿼리문에 오류가 있습니다.");
+      }
+      else{
+        res.render('index', { results: result});
+        console.log(result);
+      }
+    });
+  }else{
+    sql=sql1+sql2;
+
+    client.query(sql, function(err, result, fields){
+      if(err){
+        console.log(err);
+        console.log("쿼리문에 오류가 있습니다.");
+      }
+      else{
+        res.render('index', { results: result[1]});
+        console.log(result);
+      }
+    });
+  };
+});
+
+
+
+module.exports = router;
